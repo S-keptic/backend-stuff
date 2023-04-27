5c92118914f2343760cf892a96146b7fbbf3b09d
Anmol <anmol.volt@gmail.com>
2023-04-28 05:25:16 +0530

    fixed some more issues

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // create an http server
 
-const http = require ('http')
+const http = require ('http');
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
-console.log("server is up")
+
+
+res.setHeader("Content-Type","text/html");
+
+fs.readFile('main.html',(err,data)=>{
+    if(err){
+        console.error(err);
+    }
+    else{
+        res.end(data)
+    }
+
 })
 
-server.listen(3000,()=>{})
+
+
+
+})
+
+server.listen(3000,()=>{
+    console.log("server is up");
+})
