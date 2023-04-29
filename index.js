a280a4aabd5e5f90d74f0eefc3eb6aec576bf450
Anmol <anmol.volt@gmail.com>
2023-04-30 02:02:01 +0530

    fixed

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get("/about",(req,res)=>{
     res.sendFile("./views/index.html",{root:__dirname})
 })
 
+
+app.get("/about-us",(req,res)=>{
+    res.redirect("/about")
+})
+
+app.use((req,res)=>{
+    
+    res.status(404).sendFile("./views/404.html",{root:__dirname})
+})
+
+
 app.listen(200,()=>{
     console.log("sick")
 })
+
