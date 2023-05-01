7974f9b5240388b03d55e7f59a42141bdede3569
Anmol <anmol.volt@gmail.com>
2023-05-01 23:48:00 +0530

    34

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const postUser = ()=>{ app.post("/users",(req,res)=>{
 app.patch("/users",(req,res)=>{
 
 let updatedData;
-for(let key in users){
+for(let key in users){   
     users[key]=updatedData[key]
 
 }
