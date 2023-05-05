114762b0344938e527572249da778e91c2613ed3
Anmol <anmol.volt@gmail.com>
2023-05-06 03:16:37 +0530

    dafucl

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const chalk = require('chalk')
 const app = express();
 require('dotenv').config();
 
+app.get("/",(req,res)=>{
+    res.send("nigga")
+})
 
 
 app.listen(process.env.PORT,()=>{
-    console.log(chalk.green(`server running on ${process.env.PORT}`))
+    console.log(chalk.red(`server running on ${process.env.PORT}`))
 })
